b04d128778fc4ac1298b82ee049244daaaeac588
Shane <pheight@protonmail.com>
2021-05-15 16:06:12 -0500

    Added 404 and 500 error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,7 +138,6 @@ LOGIN_URL = 'users:login'
 
 # Heroku Settings
 import django_heroku
-import os
 django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
